5c2adcb00c855b2d3e2184141ebe4aaffb3ea280
deanaf <cooldeana@gmail.com>
2020-05-17 17:12:00 -0400

    Added ajax-loader
    Added ajax-loader

--- code/rkbc/wwwroot/js/site.js
+++ code/rkbc/wwwroot/js/site.js
@@ -17,4 +17,14 @@
             return (confirm('Are you sure you wish to delete this data?'));
         });
     });
+    //ajax-loader
+    function hideLoader() {
+        $('#loading').hide();
+    }
+    
+    $(window).ready(hideLoader);
+
+    // Strongly recommended: Hide loader after 20 seconds, even if the page hasn't finished loading
+    setTimeout(hideLoader, 20 * 1000);
 }
+$('#loading').show();
