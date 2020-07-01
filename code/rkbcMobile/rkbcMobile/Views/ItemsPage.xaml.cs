de543b23a1acd1b2763a793743f3a461dc461d01
Deana Franks <deana.franks@woodplc.com>
2020-06-30 23:05:46 -0400

    U

--- code/rkbcMobile/rkbcMobile/Views/ItemsPage.xaml.cs
+++ code/rkbcMobile/rkbcMobile/Views/ItemsPage.xaml.cs
@@ -20,37 +20,12 @@ namespace rkbcMobile.Views
     [DesignTimeVisible(false)]
     public partial class ItemsPage : ContentPage
     {
-        ItemsViewModel viewModel;
-        ViewModelLocator locator;
-        public ItemsPage(IUnitOfWork _unitOfWork, ViewModelLocator _locator)
+        public ItemsPage()
         {
             InitializeComponent();
-            locator = _locator;
-            BindingContext = viewModel = new ItemsViewModel(_unitOfWork);
+            
         }
 
-        async void OnItemSelected(object sender, EventArgs args)
-        {
-            var layout = (BindableObject)sender;
-            var item = (Item)layout.BindingContext;
-            var itemDetailVm = locator.ItemDetail;
-            itemDetailVm.Item = item;
-            var itemDetailpage = new ItemDetailPage();
-            itemDetailpage.BindingContext = itemDetailpage;
-            await Navigation.PushAsync(itemDetailpage);
-        }
-
-        async void AddItem_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
-        }
-
-        protected override void OnAppearing()
-        {
-            base.OnAppearing();
-
-            if (viewModel.Items.Count == 0)
-                viewModel.IsBusy = true;
-        }
+        
     }
 }
